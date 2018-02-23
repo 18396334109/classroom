db556dd4c752b9face555fc7bc707d6798ac5930
18396334109 <854382268@qq.com>
2018-02-23 23:57:31 +0800

    swiper

--- view/index/js/index.js
+++ view/index/js/index.js
@@ -1,30 +1,7 @@
-// $(function(){
-//     $('.marquee').bxSlider({
-//           mode:'vertical', //默认的是水平
-//           displaySlideQty:5,//显示li的个数
-//           moveSlideQty: 2,//移动li的个数  
-//           captions: true,//自动控制
-//           auto: true,
-//           controls: false//隐藏左右按钮
-//     });
-//   });
-
-
-$(document).ready(function(){
-
-    $('.slider3').bxSlider({
-
-      slideWidth: 200,
-
-      minSlides: 2,
-
-      maxSlides: 6,
-
-      moveSlides: 1,
-
-      slideMargin: 10
-
-    });
-
-  });
-
+var mySwiper = new Swiper('.swiper-container', {
+    // autoplay: true, //可选选项，自动滑动
+    direction: 'horizontal',
+    slidesPerView: 6,
+    slidesPerGroup: 6,
+    spaceBetween: 10,
+})
