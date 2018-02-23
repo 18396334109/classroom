d0c6266924980ded01d2ffa9b316caf569630f36
18396334109 <854382268@qq.com>
2018-02-23 18:18:53 +0800

    Initial commit

---
+++ view/index/js/index.js
@@ -0,0 +1,30 @@
+// $(function(){
+//     $('.marquee').bxSlider({
+//           mode:'vertical', //默认的是水平
+//           displaySlideQty:5,//显示li的个数
+//           moveSlideQty: 2,//移动li的个数  
+//           captions: true,//自动控制
+//           auto: true,
+//           controls: false//隐藏左右按钮
+//     });
+//   });
+
+
+$(document).ready(function(){
+
+    $('.slider3').bxSlider({
+
+      slideWidth: 200,
+
+      minSlides: 2,
+
+      maxSlides: 6,
+
+      moveSlides: 1,
+
+      slideMargin: 10
+
+    });
+
+  });
+
