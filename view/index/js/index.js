5d32705e47322deb9d6f4a050fe070bde68f8c6d
18396334109 <854382268@qq.com>
2018-02-26 22:44:23 +0800

    homework

--- view/index/js/index.js
+++ view/index/js/index.js
@@ -2,11 +2,11 @@ $(function() {
     $('[data-toggle="tooltip"]').tooltip();
     getWindowHeight();
     //首页大事记
-    $('.course_nr li').hover(function(){
+    $('.course_nr li').hover(function() {
 
         $(this).find('.shiji').slideDown(600);
 
-    },function(){
+    }, function() {
 
         $(this).find('.shiji').slideUp(400);
 
@@ -15,19 +15,19 @@ $(function() {
 
 
 function getWindowHeight() {
-    var blackboard = screen.height * 0.63;
-    var infoPanel = screen.height * 0.38;
-    var chatPanel = screen.height * 0.25;
-    $(".text-img").css("height", blackboard);
-    $("#StudentList").css("min-height", infoPanel);
-    $("#TodayFocus").css("min-height", infoPanel);
-    $(".chatBody").css("min-height", chatPanel);
+    // var blackboard = screen.height * 0.6;
+    // var infoPanel = screen.height * 0.35;
+    // var chatPanel = screen.height * 0.25;
+    // $(".text-img").css("height", blackboard);
+    // $("#StudentList").css("min-height", infoPanel);
+    // $("#TodayFocus").css("min-height", infoPanel);
+    // $(".chatBody").css("min-height", chatPanel);
 }
 
 function sendMessage() {
     var Message = $("#Message").val();
     var time = new Date();
-    var data = '<p style="text-align:center">'+ time.toLocaleDateString()+time.toLocaleTimeString()+'</p>'+'<p>' + Message + '</p>';
+    var data = '<p style="text-align:center">' + time.toLocaleDateString() + time.toLocaleTimeString() + '</p>' + '<p>' + Message + '</p>';
     $(".chatBody").append(data);
     $("#Message").val('');
 }
